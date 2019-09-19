c6c86da92c371bd350f4bc57a65ccf6bab8d2f3f
Sachin <jadhavsachin174@gmail.com>
2019-09-19 19:24:29 +0530

    adding orientation change event 1

--- js/events.js
+++ js/events.js
@@ -91,6 +91,7 @@ function handleOrientation(event) {
 
     if (player && player.length > 0) {
         player[selectedPlayerIndex].update((maxX * x / 180 - 10), (maxY * y / 180 - 10));
+        player[selectedPlayerIndex].render();
         document.getElementById("xy").innerHTML = (maxX * x / 180 - 10) + " " + (maxY * y / 180 - 10);
         alert((maxX * x / 180 - 10) + " " + (maxY * y / 180 - 10));
     }
